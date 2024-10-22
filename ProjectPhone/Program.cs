bf17d56e6406ed4c82428a9f3a70e9d9ae163f26
p. yankhaev_nalunch <p.yankhaev@nalunch.ru>
2024-10-22 22:16:08 +0700

    example

---
+++ ProjectPhone/Program.cs
@@ -0,0 +1,56 @@
+﻿namespace ProjectPhone
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TestData testData = new TestData();
+            Console.WriteLine("Введите размер количества номеров телефона, которые вы хотите сгенерировать");
+            var amount = EnterNumbers();
+            Generate(testData, amount);
+            Console.WriteLine($"Сгенерировано {testData.phones.Count} номеров телефона:");
+            foreach (var c in testData.phones)
+            {
+                Console.WriteLine(c);
+            }
+        }
+
+        static long RandPhone()
+        {
+            Random random = new Random();
+            return (89000000000 + random.Next(1, 999999999));
+        }
+
+        static void AddRandom(TestData td)
+        {
+            td.phones.Add(RandPhone());
+        }
+
+        public static object Generate(TestData td, int n)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                AddRandom(td);
+            }
+            return td;
+        }
+        static int EnterNumbers()
+        {
+            int result;
+            while (!int.TryParse(Console.ReadLine(), out result))
+            {
+                Console.WriteLine("Введите целое число");
+            }
+            return result;
+        }
+    }
+
+    public struct TestData 
+    {
+        public List<long> phones = new List<long>();
+
+        public TestData()
+        {
+        }
+    }
+}
---
+++ ProjectPhone/Tests.cs
@@ -0,0 +1,34 @@
+﻿using NUnit;
+using NUnit.Framework.Internal.Execution;
+
+namespace ProjectPhone
+{
+    [TestFixture]
+    public class Tests
+    {
+        //Можно ли запускать тесты асинхронно?
+        [Test]
+        public async Task CreateThreeNumbers() 
+        {
+            TestData td = new TestData();
+            Program.Generate(td, 3);
+            Assert.That(td.phones.Count, Is.EqualTo(3));
+        }
+
+        [Test]
+        public async Task CreateZeroNumbers() 
+        {
+            TestData td = new TestData();
+            Program.Generate(td, 0);
+            Assert.That(td.phones.Count, Is.EqualTo(0));
+        }
+
+        [Test]
+        public async Task EnterNegativeCountNumber() 
+        {
+            TestData td = new TestData();
+            Program.Generate(td, -1);
+            Assert.That(td.phones.Count, Is.EqualTo(0));
+        }
+    }
+}
